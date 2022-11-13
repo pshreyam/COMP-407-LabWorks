04d5838a8c45d8131c4ad9541cb899b54630fcc1
Shreyam Pokharel <pshreyam@gmail.com>
2022-11-13 16:21:55 +0545

    Update lab3.m to show all plots using subplot

--- lab3.m
+++ lab3.m
@@ -7,6 +7,8 @@ f = 2000 % Signal Frequency, 2KHz
 T = 1 / f % Signal Time Period
 t = 0 : 0.000001 : cycles * T
 x = 5 * sin(2 * pi * f * t)
+
+subplot(4, 1, 1)
 plot(t, x)
 legend("x = 5sin(2πft)")
 title("Signal x = 5sin(2πft) with 5 cycles and f = 2kHz")
@@ -23,6 +25,8 @@ sampling_frequency = 5000 % F_s = 5 KHz
 sampling_time_period = 1 / sampling_frequency
 sampling_time_vector = 0 : sampling_time_period : cycles * T
 sample_amplitude = 5 * sin(2 * pi * f * sampling_time_vector)
+
+subplot(4, 1, 2)
 plot(t, x)
 legend("x = 5sin(2πft)")
 hold on;
@@ -41,6 +45,8 @@ sampling_frequency = 10000 % F_s = 10 KHz
 sampling_time_period = 1 / sampling_frequency
 sampling_time_vector = 0 : sampling_time_period : cycles * T
 sample_amplitude = 5 * sin(2 * pi * f * sampling_time_vector)
+
+subplot(4, 1, 3)
 plot(t, x)
 legend("x = 5sin(2πft)")
 hold on;
@@ -59,6 +65,8 @@ sampling_frequency = 20000 % F_s = 20 KHz
 sampling_time_period = 1 / sampling_frequency
 sampling_time_vector = 0 : sampling_time_period : cycles * T
 sample_amplitude = 5 * sin(2 * pi * f * sampling_time_vector)
+
+subplot(4, 1, 4)
 plot(t, x)
 legend("x = 5sin(2πft)")
 hold on;
@@ -76,6 +84,8 @@ f = 2000 % Signal Frequency, 2KHz
 T = 1 / f % Signal Time Period
 t = 0 : 0.000001 : cycles * T
 x = 5 * cos(2 * pi * f * t)
+
+subplot(4, 1, 1)
 plot(t, x)
 legend("x = 5cos(2πft)")
 title("Signal x = 5cos(2πft) with 3 cycles and f = 2kHz")
@@ -92,6 +102,8 @@ sampling_frequency = 5000 % F_s = 5 KHz
 sampling_time_period = 1 / sampling_frequency
 sampling_time_vector = 0 : sampling_time_period : cycles * T
 sample_amplitude = 5 * cos(2 * pi * f * sampling_time_vector)
+
+subplot(4, 1, 2)
 plot(t, x)
 legend("x = 5cos(2πft)")
 hold on;
@@ -110,6 +122,8 @@ sampling_frequency = 10000 % F_s = 10 KHz
 sampling_time_period = 1 / sampling_frequency
 sampling_time_vector = 0 : sampling_time_period : cycles * T
 sample_amplitude = 5 * cos(2 * pi * f * sampling_time_vector)
+
+subplot(4, 1, 3)
 plot(t, x)
 legend("x = 5cos(2πft)")
 hold on;
@@ -128,6 +142,8 @@ sampling_frequency = 20000 % F_s = 20 KHz
 sampling_time_period = 1 / sampling_frequency
 sampling_time_vector = 0 : sampling_time_period : cycles * T
 sample_amplitude = 5 * cos(2 * pi * f * sampling_time_vector)
+
+subplot(4, 1, 4)
 plot(t, x)
 legend("x = 5cos(2πft)")
 hold on;
